a0b4f87386ed4adde673eed78b138197a423289b
cbc5y <ccastaneda120@gmail.com>
2020-11-22 18:52:15 -0600

    updated yml file and changed ec2 ip

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '54.172.30.113', //double check to MAKE SURE IP IS CORreCT !!!
+      host: '54.87.23.61', //double check to MAKE SURE IP IS CORreCT !!!
       username: 'ubuntu',
       privateKey: 'hands-on.pem'
     })
